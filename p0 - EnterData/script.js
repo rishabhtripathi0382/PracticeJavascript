697a7bf681495408950e54a4b8092fe26c5ad386
Rishabh <rishabhotripathi@outlook.com>
2024-07-26 19:30:07 +0530

    p0 - EnterData & practice

--- p0 - EnterData/script.js
+++ p0 - EnterData/script.js
@@ -35,22 +35,26 @@ function deleteItem(index){
 }
 
 
-
 //1 .Write a JavaScript function to calculate the sum of two numbers. 
     let ans;
     function addTwoNumber(number1,number2){
         return number1+number2;
     }
-    // ans = addTwoNumber(1,3)
+    ans = addTwoNumber(1,3)
+
+    let answerDiv = document.querySelector('.answer')
+    showoutput = (ans) => {
+        answerDiv.textContent = ans;
+}
+
+showoutput(ans);
+console.log("outPut : ",ans);
+
 
 
 // 2. Write a JavaScript program to find the maximum number in an array. 
-    function findMaxNumber(numberArray){
-    return Math.max(...numberArray) //spread operator
-    }
-    // ans =findMaxNumber([1,4,5])
+//     function findMaxNumber(numberArray){
+//     return Math.max(...numberArray) //spread operator
+//     }
+//     ans =findMaxNumber([1,4,5])
 
-
-        
-
-console.log("outPut : ",ans);
